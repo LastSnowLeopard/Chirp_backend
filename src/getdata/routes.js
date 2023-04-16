3a59c5f565d71eb7fe38795b837a120684309389
LastSnowLeopard <forevergowith@gmail.com>
2023-04-16 12:02:17 +0500

    ok

--- src/getdata/routes.js
+++ src/getdata/routes.js
@@ -1,16 +1,16 @@
 const express = require("express")
-const route = express.Router()
+const Router = express.Router()
 
 const dataController = require('./controller/dataController') 
 const upload= require('../../helper/imageUploader');
 
 // const {auth,verifyOrigin} = require("../../helper/auth")
 
-route.post('/add-hobby-in-list',dataController.addHobbyInList)
+Router.post('/add-hobby-in-list',dataController.addHobbyInList)
 
-route.get('/get-hobby-list',dataController.getHobbyList)
+Router.get('/get-hobby-list',dataController.getHobbyList)
 
-route.get('/get-events-list',dataController.getEventList)
-route.get('/get-feelings-list',dataController.getFeelingList)
+Router.get('/get-events-list',dataController.getEventList)
+Router.get('/get-feelings-list',dataController.getFeelingList)
 
-module.exports = route
+module.exports = Router
